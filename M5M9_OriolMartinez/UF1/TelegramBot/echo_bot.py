f689bd3babc6bb7743688f94e6c98dffaeb92803
oriol_martinez <oriolmartinez2@gmail.com>
2019-10-23 17:04:10 +0200

    .

--- M5M9_OriolMartinez/UF1/TelegramBot/echo_bot.py
+++ M5M9_OriolMartinez/UF1/TelegramBot/echo_bot.py
@@ -1,12 +1,11 @@
 import telebot
 import webbrowser
-import pyowm
-
+import requests
 bot = telebot.TeleBot("850906969:AAEggI372vsBtM84ozbxRIKKBHJGEvWqRuQ")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Soy tu hombre del tiempo, preguntame!")
+	bot.reply_to(message, "Soy tu hombre del tiempo, preguntame! Como funciono? Introduce el comando /tiempo i seguidamente el stitio")
 
 
 
@@ -15,17 +14,20 @@ def send_welcome(message):
     #bot.reply_to(message, )
     lugar = message.text.split()
     print (lugar[1])
-
+    response = requests.get("https://www.eltiempo.es/"+lugar[1]+".html")
+    print(response.text)
+    temp = response.text[100:103]
+    print temp
     webbrowser.open("https://www.eltiempo.es/"+lugar[1]+".html", new=2, autoraise=True)
 
 @bot.message_handler(commands=['neifi'])
 def send_welcome(message):
     #bot.reply_to(message, )
 
-    own = pyowm.owM
+    lugar = message.text.split()
     
 
-   
-
+    webbrowser.open("https://www.netflix.com/es/", new=2, autoraise=True)    
+    
 bot.polling() 
 
