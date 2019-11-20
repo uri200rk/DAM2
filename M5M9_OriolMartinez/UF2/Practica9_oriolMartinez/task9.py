f8136aa0d0917631dc2dc815732baf9c9e2df1ef
oriol_martinez <oriolmartinez2@gmail.com>
2019-11-20 19:46:45 +0100

    .

--- M5M9_OriolMartinez/UF2/Practica9_oriolMartinez/task9.py
+++ M5M9_OriolMartinez/UF2/Practica9_oriolMartinez/task9.py
@@ -15,7 +15,9 @@ def primers(num):
 
 if __name__ == '__main__':
 
-    pool = Pool(2)
+    pool = Pool(2)  # el numero indica quants procesos es fan com a màxim, surt a compte sempre i quan no es pasis de procesos actius, depenent del processador
+                    # del pc, admetrà més processos o menys, ja que si forçem a fer més processos del que pot, es probable que no pugui. L'avantatge d'incloure més
+                    # d'un process es que el programa s'acabarà abans, ja que els processos es fan a la vegada.
 
     l = range(40000000, 40000100)#[45445535, 56534563, 43566487, 43635453, 52346557, 53454433, 43546453, 26847357, 54577647]
     start = datetime.now()
