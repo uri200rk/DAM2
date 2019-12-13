156b1dfe3c29a43da9f515513b33e836f1e4bf3e
oriol_martinez <oriolmartinez2@gmail.com>
2019-12-13 19:55:29 +0100

    ·

--- dollarDuckAPP/dollarDuck.js
+++ dollarDuckAPP/dollarDuck.js
@@ -1,10 +1,8 @@
 
 
-// client id  201907890850-2mr51j065r2m1c2id16trkotq7go7pd5.apps.googleusercontent.com 
+$(document).ready(function(){
+    $('.sidenav').sidenav();
 
-// api key AIzaSyCPyzW_SVgl6ZDNO09f8Zq3eCNYNZCTC3M 
-//key = AIzaSyCPyzW_SVgl6ZDNO09f8Zq3eCNYNZCTC3M
-$(function(){
     var url2= "https://sheets.googleapis.com/v4/spreadsheets/1fbqJKI7A-W6G1ubuxS5ScDp-suE3k7y9YoP-LdR5lYg/values/";
 	url2 += "oriolDB!A1:B1?majorDimension=ROWS&key=AIzaSyCPyzW_SVgl6ZDNO09f8Zq3eCNYNZCTC3M";
 
