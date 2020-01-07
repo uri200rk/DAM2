8c041b81c446140c713ff053824bd0f493144900
uri200rk <oriolmartinez2@gmail.com>
2020-01-07 19:59:10 +0100

    .
    .

--- dollarDuckAPP/dollarDuck.js
+++ dollarDuckAPP/dollarDuck.js
@@ -1,20 +1,20 @@
 
-
 $(document).ready(function(){
-    $('.sidenav').sidenav();
+    
 
     var url2= "https://sheets.googleapis.com/v4/spreadsheets/1fbqJKI7A-W6G1ubuxS5ScDp-suE3k7y9YoP-LdR5lYg/values/";
 	url2 += "oriolDB!A1:B1?majorDimension=ROWS&key=AIzaSyCPyzW_SVgl6ZDNO09f8Zq3eCNYNZCTC3M";
 
     $.get(url2,function(dades){
+
+        $( ".eventos" ).prepend(JSON.stringify(dades));
+       
         
         console.log(JSON.stringify(dades));
     });
 
 
     
-        $(".boton").click(function(){
-            $("#formulari").slideToggle();
             
            
          
