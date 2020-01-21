d3d222c3808a2e092b811c58a01665f205767000
oriol_martinez <oriolmartinez2@gmail.com>
2020-01-21 19:54:08 +0100

    .

--- M5M9_OriolMartinez/UF3/Practica16_OriolMartinez/client-speak.py
+++ M5M9_OriolMartinez/UF3/Practica16_OriolMartinez/client-speak.py
@@ -7,10 +7,25 @@ PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 
-#rebem el misatge
-t=s.recv(1024)
-#printem el misatge 
-print t
+
+
+def recibir(socket):
+    #recibir mensaje
+    mensaje = socket.recv(1024)
+    print mensaje
     
+
+def enviar(socket,data):
+    #misatge a enviar
+    socket.sendall(data)
+    
+
+while True:
+   
+    #entramos parametro por teclado
+    data = raw_input()
+    enviar(s,data)
+    recibir(s)
+
 s.close()
 
--- M5M9_OriolMartinez/UF3/Practica16_OriolMartinez/server-listen.py
+++ M5M9_OriolMartinez/UF3/Practica16_OriolMartinez/server-listen.py
@@ -1,18 +1,42 @@
 # Echo server program
 import socket
 import time
+import threading
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST,PORT))
+
+
 # marquem cuantes conexions esperem
 s.listen(1)
 #acceptem la conexio
 conn, addr = s.accept()
-#misatge a enviar
-conn.sendall("conectado exitosamente!")
 
 
+def recibir(socket):
+    #recibir mensaje
+    data = socket.recv(1024)
+    print data
+
+def enviar(socket,data):
+    #misatge a enviar
+    socket.sendall(data)
+
+
+    
+
+while True:
+    t.start()
+    #entramos parametro por teclado
+    data = raw_input()
+    t2.start()
+
+t = threading.Thread(target=recibir, args=(conn,))
+t2 = threading.Thread(target=enviar, args=(conn, data))
 
 s.close()
+
+
+
